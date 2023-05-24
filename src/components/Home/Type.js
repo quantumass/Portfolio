bd12489bd67ab6bfeea8e5035ed5d6a1f0c9f45e
Soufiane Masmoud <masmoud@live.com>
2023-05-24 22:58:34 +0100

    fixed the typing options

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,22 +1,22 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
+import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    'Software Developer',
+                    'IOS Developer',
+                    'Devops engineer',
+                    'Linux Lover',
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+            }}
+        />
+    )
 }
 
-export default Type;
+export default Type
